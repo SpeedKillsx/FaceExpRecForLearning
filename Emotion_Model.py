e2c0756a56f5035aa52876594dd3f5d65e33dcab
SpeedKillsx <amayaslabchri88@gmail.com>
2021-04-28 15:22:26 +0100

    comit on Emorion_model

--- Emotion_Model.py
+++ Emotion_Model.py
@@ -1,5 +1,9 @@
 import os
-import tensorflow
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # To hide tensorflow warning's
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2 as cv
 import pandas as pd
+
+# Charger le dataset
+# utilisation de FER-2013 comme datasets
